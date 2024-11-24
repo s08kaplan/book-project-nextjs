4a6f8ff8759d9a2a48c8e417a9a1a2cdb43ee221
Serkan <s8ersis@gmail.com>
2024-11-24 21:34:26 +0300

    update: cors policy handled for post book

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 
 dbConnection();
 
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': 'http://localhost:3000', 
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
 export async function GET(req: Request) {
   try {
     return await getBooks(req);
@@ -21,22 +26,14 @@ export async function POST(req: Request) {
 
     return NextResponse.json(result, {
       status: 201,
-      headers: {
-        "Access-Control-Allow-Origin": "http://127.0.0.1:3000",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
+      headers: CORS_HEADERS,
     });
   } catch (error) {
     return NextResponse.json(
       { error: true, message: (error as Error).message },
       {
         status: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "http://127.0.0.1:3000",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
+        headers: CORS_HEADERS,
       }
     );
   }
@@ -47,11 +44,7 @@ export function OPTIONS() {
   return NextResponse.json(
     {},
     {
-      headers: {
-        "Access-Control-Allow-Origin": "http://127.0.0.1:3000",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
+      headers: CORS_HEADERS,
     }
   );
 }
